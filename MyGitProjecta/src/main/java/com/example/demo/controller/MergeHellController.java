4ae91d6ffb0613722492415301730bec19b8c8db
S0603 <S0603@22S0603>
2023-11-14 15:07:43 +0900

    C

---
+++ MyGitProjecta/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+	}
+	//void kadai〇(){
+	//ここに課題を書く。メソッド名変えてね。
+	//}
+
+	private static void kadaiC() {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 50;
+		int b = 70;
+		int c = (a+b);
+		
+		System.out.print(c);
+	}
+}

1f3b36b9c1b85186acfeef7aec6567be43102aaf
S0623 <S0623@22S0623>
2023-11-14 15:08:14 +0900

    aaa

---
+++ MyGitProjecta/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA( int Count = 10;
+	            for (int i = 1; i <= loopCount; i++) {
+	                  System.out.println("現在" + i + "回目のループです");
+	        });
+		kadaiB();
+		kadaiC();
+	}
+	//void kadai〇(){
+	//ここに課題を書く。メソッド名変えてね。
+	//}
+}
